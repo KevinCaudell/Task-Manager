1b30e19cbab586f96c239d63ca4876901f27d949
Kevin Caudell <kevin.caudell.1@gmail.com>
2026-01-08 20:59:50 -0500

    Add routes for calendar, dashboard, login, settings, and tasks; update index.html links for signup and login

--- app.py
+++ app.py
@@ -6,6 +6,26 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/calendar')
+def calendar():
+    return render_template('calendar.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/settings')
+def settings():
+    return render_template('settings.html')
+
+@app.route('/tasks')
+def tasks():
+    return render_template('tasks.html')
+
 
 
 if __name__ == '__main__':
