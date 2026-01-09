9019f2373974a641a16d41f08d576e686ccd77d9
Kevin Caudell <kevin.caudell.1@gmail.com>
2026-01-09 14:24:09 -0500

    Add SQLAlchemy models for Users and Tasks with autoload configuration

---
+++ models.py
@@ -0,0 +1,22 @@
+from app import db
+
+class User(db.Model):
+    __tablename__ = 'USER'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+
+    tasks = db.relationship('Task', backref='user', lazy=True)  
+
+class Task(db.Model):
+    __tablename__ = 'TASK'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    category = db.Column(db.String(20), nullable=False)
+    description = db.Column(db.Text)
+    priority = db.Column(db.String(20), nullable=False)
+    due_date = db.Column(db.Date, nullable=False)
+    color = db.Column(db.String(20), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('USER.ID'), nullable=False)
