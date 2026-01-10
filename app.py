38155fa30b13f0c2a1eeb645515e7140275de59d
Kevin Caudell <kevin.caudell.1@gmail.com>
2026-01-09 22:36:20 -0500

    Refactor app routes and database initialization; rename functions for clarity and ensure proper app context

--- app.py
+++ app.py
@@ -1,45 +1,47 @@
 from flask import Flask, render_template
-import os
-from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
+from extensions import db   
+import os
 
 load_dotenv()
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASS')}@localhost/{os.getenv('DB_NAME')}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
-db = SQLAlchemy(app)
+db.init_app(app)
 
-from models import Users, Tasks
+from models import User, Task
 
 @app.route('/')
-def home():
+def home_page():
     return render_template('index.html')
 
 @app.route('/calendar')
-def calendar():
+def calendar_page():
     return render_template('calendar.html')
 
 @app.route('/dashboard')
-def dashboard():
+def dashboard_page():
     return render_template('dashboard.html')
 
 @app.route('/login')
-def login():
+def login_page():
     return render_template('login.html')
 
 @app.route('/settings')
-def settings():
+def settings_page():
     return render_template('settings.html')
 
 @app.route('/tasks')
-def tasks():
-    return render_template('tasks.html')
+def tasks_page():
+    all_tasks = Task.query.all()
+    return render_template('tasks.html', tasks=all_tasks)
 
 @app.route('/signup')
-def signup():
+def signup_page():
     return render_template('signup.html')
 
 
